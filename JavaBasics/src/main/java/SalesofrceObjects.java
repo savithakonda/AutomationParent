1a2bb589df2113449a48862c093b1ec79215eed6
skonda23 <savitha.konda@gmail.com>
2020-03-30 13:21:34 -0700

    Created new class called Objects

---
+++ JavaBasics/src/main/java/SalesofrceObjects.java
@@ -0,0 +1,8 @@
+public class SalesofrceObjects {
+
+    public static void main(String[] args){
+        System.out.println("Leads, Accounts, Opportunities, Users");
+        System.out.println("Login.salesforce.com");
+
+    }
+}
