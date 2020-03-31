eec08dd20abe35e66e5ac375389b5c45ccde3140
skonda23 <savitha.konda@gmail.com>
2020-03-31 11:09:08 -0700

    Created new class called New

---
+++ JavaBasics/src/main/java/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+
+    static int c=30;
+    int b=20;
+    public static void main(String[] args){
+        int a=10;
+        System.out.println(c);
+        System.out.println(a);
+        Variable obj=new Variable();
+        System.out.println(obj.b);
+
+    }
+
+}
